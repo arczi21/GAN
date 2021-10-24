759a974a4675f1aae68b7faecac82e74337ecdfc
arczi21 <89776102+arczi21@users.noreply.github.com>
2021-10-24 14:06:29 +0200

    Update test images generator

--- test.py
+++ test.py
@@ -19,8 +19,12 @@ y = 10
 noise = distribution.sample((x * y, noise_size, 1, 1)).to(device)
 img = g(noise).detach().cpu().numpy()
 
-f, axarr = plt.subplots(x, y)
+f, ax = plt.subplots(x, y)
 for i in range(x):
     for j in range(y):
-        axarr[i, j].imshow(img[i * y + j, 0], cmap='gray')
+        ax[i, j].imshow(img[i * y + j, 0], cmap='gray')
+        ax[i, j].set_xticks([])
+        ax[i, j].set_yticks([])
+
+
 plt.savefig('generated.png')
